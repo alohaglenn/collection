78394fc2739d5f76b5795ca2bcd8deed1ed8daff
alohaglenn <glenngonda@gmail.com>
2015-10-19 10:07:20 -0700

    adding app routes

--- public/app.js
+++ public/app.js
@@ -4,3 +4,17 @@
 
 var app = angular.module('collection', ['ngRoute', 'ngResource']);
 
+  ///////////////////
+ //    ROUTES     //
+///////////////////
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '',
+      controller: ''
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
