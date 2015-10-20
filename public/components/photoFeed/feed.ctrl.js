40239b6cdd99e66a3248aafd528d0296690b332c
alohaglenn <glenngonda@gmail.com>
2015-10-19 21:07:53 -0700

    refactoring API call on the feed component

--- public/components/photoFeed/feed.ctrl.js
+++ public/components/photoFeed/feed.ctrl.js
@@ -1,12 +1,29 @@
 angular.module('collection')
 
-.controller('feedCtrl', ['$scope', 'feedService', 'homeService', function($scope, feedService, homeService) {
+.controller('feedCtrl', ['$scope', '$resource', 'APIcred', 'homeService', function($scope, $resource, APIcred, homeService) {
 
  $scope.tag = homeService.tag;
 
  $scope.photos = [];
 
- feedService.getResults(function(data) {
+ var getResults = function(cbk) {
+      var api = $resource('https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent?client_id=client_id&callback=JSON_CALLBACK', 
+        {
+          client_id: APIcred.CLIENT_ID,
+        },
+        {
+        get: {
+          method: 'JSONP', 
+          params: { q: 'hellothere' }
+        }
+      });
+
+      api.get(function(res) {
+        cbk(res.data);
+      });
+ };
+
+ getResults(function(data) {
   $scope.photos = data;
   console.log('photos: ',$scope.photos);
  });
--- public/components/photoFeed/feed.service.js
+++ public/components/photoFeed/feed.service.js
@@ -1,24 +1,26 @@
-angular.module('collection')
+// angular.module('collection')
 
-.service('feedService', function($resource, APIcred, homeService) {
+// .service('feedService', function($resource, APIcred) {
 
-  return {
-    getResults: function(cbk) {
-      var tagName;
-      var api = $resource('https://api.instagram.com/v1/tags/' + tagName + '/media/recent?client_id=client_id&callback=JSON_CALLBACK', 
-        {
-          client_id: APIcred.CLIENT_ID,
-        },
-        {
-        get: {
-          method: 'JSONP'
-        }
-      });
+//   return {
+//     getResults: function(cbk) {
+//       var tagName;
+//       $scope = '';
+//       var api = $resource('https://api.instagram.com/v1/tags/' + tagName + '/media/recent?client_id=client_id&callback=JSON_CALLBACK', 
+//         {
+//           client_id: APIcred.CLIENT_ID,
+//         },
+//         {
+//         get: {
+//           method: 'JSONP', 
+//           params: { q: 'hellothere' }
+//         }
+//       });
 
-      api.get(function(res) {
-        cbk(res.data);
-      });
-    }
-  };
+//       api.get(function(res) {
+//         cbk(res.data);
+//       });
+//     }
+//   };
 
-});
+// });
