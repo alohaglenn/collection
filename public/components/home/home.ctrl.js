fa8afe4ec8607c5785bd31ab3db182aa5ae8e1af
alohaglenn <glenngonda@gmail.com>
2015-10-20 17:15:34 -0700

    adding a background image and styling on home view

--- public/components/home/home.ctrl.js
+++ public/components/home/home.ctrl.js
@@ -2,6 +2,8 @@ angular.module('collection')
 
 .controller('homeCtrl', ['$scope', 'homeService', function($scope, homeService) {
 
+  $scope.bgimg = './assets/ig.jpg';
+
   $scope.tag = homeService.tag;
 
   $scope.$watch('tag', function() {
