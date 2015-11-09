2cf2d72d87fea4b93a963883b39744c068dee725
alohaglenn <glenngonda@gmail.com>
2015-11-08 19:46:45 -0800

    implementing local server functionality

--- public/components/photoFeed/feed.ctrl.js
+++ public/components/photoFeed/feed.ctrl.js
@@ -1,6 +1,6 @@
 angular.module('collection')
 
-.controller('feedCtrl', ['$scope', '$http', '$resource', 'homeService', function($scope, $http, $resource, homeService) {
+.controller('feedCtrl', ['$scope', '$http', '$resource', 'APIcred', 'homeService', function($scope, $http, $resource, APIcred, homeService) {
 
  $scope.tag = homeService.tag; //binds data entered in search bar on home page
 
@@ -15,7 +15,7 @@ angular.module('collection')
     // var nextMaxID = $scope.nextMaxID; //need for pagination later 
     var api = $resource('https://api.instagram.com/v1/tags/' + tagName + '/media/recent?client_id=client_id&callback=JSON_CALLBACK', 
       {
-        client_id: '82df4d469f0f4e7f82ac8a7e3226c907', //grabs CLIENT_ID from APIcred.js
+        client_id: APIcred.CLIENT_ID, //grabs CLIENT_ID from APIcred.js
       },
       {
       get: {
